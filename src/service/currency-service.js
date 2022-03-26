d92dfb609273f10c7e024c9840ef9d210f66da42
Yazmin Perez <zoeperez333@gmail.com>
2022-03-25 19:39:51 -0700

    update to async & await

--- src/service/currency-service.js
+++ src/service/currency-service.js
@@ -1,14 +1,15 @@
-export default class CurrencyExchange {  
-  static getCurrency(input, amount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${input}/${amount}`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      })
+export default class CurrencyExchange {
+  static async getCurrency(currencyOne, currencyTwo) {
+    try {
+      const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyOne}/${currencyTwo}`
+      );
+      if (!response.ok) {
+        throw Error(response.status);
+      }
+      return await response.json();
+    } catch (error) {
+      return error.message;
+    }
   }
 }
