00ae082a55a90d85fd2e2e49a6fe8552b3600531
Yazmin Perez <zoeperez333@gmail.com>
2022-03-25 17:28:12 -0700

    add basic js outline

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import CurrencyExchange from './service/currency-service';
+
+
+function clearFields() {
+  $("").text("");
+  $("").text("");
+  $("").text("");
+}
+
+function getElements(response) {
+  if () {
+    
+  } else {
+
+  }
+}
+
+async function makeApiCall(currencyOne, currencyTwo) {
+  const response = await CurrencyExchange.getCurrency(currencyOne, currencyTwo);
+  getElements(response);
+}
+
+$(document).ready(function () {
+  $("#convertButton").click(function () {
+    let currencyOne = $("#currencyOne").val();
+    let currencyTwo = $("#currencyTwo").val();
+    clearFields();
+    makeApiCall(currencyOne, currencyTwo);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
